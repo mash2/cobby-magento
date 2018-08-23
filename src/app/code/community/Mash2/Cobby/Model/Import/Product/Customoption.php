3679c2040c33a8a25ea8c043ebf0e4228a0ab470
sl4d1c <slawa.minden@freenet.de>
2018-08-23 13:54:14 +0200

    splited add and delete

--- src/app/code/community/Mash2/Cobby/Model/Import/Product/Customoption.php
+++ src/app/code/community/Mash2/Cobby/Model/Import/Product/Customoption.php
@@ -39,6 +39,7 @@ class Mash2_Cobby_Model_Import_Product_Customoption extends Mash2_Cobby_Model_Im
         Mage::dispatchEvent('cobby_import_product_customoption_import_before', array( 'products' => $productIds ));
 
         $items = array();
+        $remove = array();
         foreach($rows as $productId => $productCustomOptions) {
             if (!in_array($productId, $existingProductIds))
                 continue;
@@ -59,100 +60,121 @@ class Mash2_Cobby_Model_Import_Product_Customoption extends Mash2_Cobby_Model_Im
                 'values_prices' => array()
             );
 
-            foreach($productCustomOptions as $productCustomOption) {
-                if(isset($productCustomOption['option_id'])) {
-                    $nextOptionId = $productCustomOption['option_id'];
-                }else {
-                    $nextOptionId = $nextAutoOptionId++;
-                }
-
-                $items[$productId]['options'][] = array(
-                    'option_id'      => $nextOptionId,
-                    'sku'            => $productCustomOption['sku'],
-                    'max_characters' => $productCustomOption['max_characters'],
-                    'file_extension' => $productCustomOption['file_extension'],
-                    'image_size_x'   => $productCustomOption['image_size_x'],
-                    'image_size_y'   => $productCustomOption['image_size_y'],
-                    'product_id'     => $productId,
-                    'type'           => $productCustomOption['type'],
-                    'is_require'     => $productCustomOption['is_require'],
-                    'sort_order'     => $productCustomOption['sort_order'],
-                );
-
-                $items[$productId]['product']['has_options'] = 1;
-                if($productCustomOption['is_require'] == 1) { //if one is required, product should be set to required
-                    $items[$productId]['product']['required_options'] = 1;
-                }
-
-                foreach($productCustomOption['titles'] as $productCustomOptionTitle) {
-                    $items[$productId]['titles'][] = array(
-                        'option_id' => $nextOptionId,
-                        'store_id' => $productCustomOptionTitle['store_id'],
-                        'title' => $productCustomOptionTitle['title']
-                    );
-                }
-
-                foreach($productCustomOption['prices'] as $productCustomOptionPrice) {
-                    $items[$productId]['prices'][] = array(
-                        'option_id' => $nextOptionId,
-                        'store_id' => $productCustomOptionPrice['store_id'],
-                        'price' => $productCustomOptionPrice['price'],
-                        'price_type' => $productCustomOptionPrice['price_type']
-                    );
-                }
-
-                foreach($productCustomOption['values'] as $value) {
-
-                    if(isset($value['option_type_id'])){
-                        $nextValueId = $value['option_type_id'];
-                    } else {
-                        $nextValueId = $nextAutoValueId++;
+            if( $productCustomOptions['add'] ){
+                foreach($productCustomOptions['add'] as $productCustomOption) {
+                    if(isset($productCustomOption['option_id'])) {
+                        $nextOptionId = $productCustomOption['option_id'];
+                    }else {
+                        $nextOptionId = $nextAutoOptionId++;
                     }
 
-                    $items[$productId]['values'][] = array(
-                        'option_type_id' => $nextValueId,
-                        'option_id' => $nextOptionId,
-                        'sku' => $value['sku'],
-                        'sort_order' => $value['sort_order']
+                    $items[$productId]['options'][] = array(
+                        'option_id'      => $nextOptionId,
+                        'sku'            => $productCustomOption['sku'],
+                        'max_characters' => $productCustomOption['max_characters'],
+                        'file_extension' => $productCustomOption['file_extension'],
+                        'image_size_x'   => $productCustomOption['image_size_x'],
+                        'image_size_y'   => $productCustomOption['image_size_y'],
+                        'product_id'     => $productId,
+                        'type'           => $productCustomOption['type'],
+                        'is_require'     => $productCustomOption['is_require'],
+                        'sort_order'     => $productCustomOption['sort_order'],
                     );
 
-                    foreach($value['titles'] as $valueTitle) {
-                        $items[$productId]['values_titles'][] = array(
-                            'option_type_id' => $nextValueId,
-                            'store_id' => $valueTitle['store_id'],
-                            'title' => $valueTitle['title'],
+                    $items[$productId]['product']['has_options'] = 1;
+                    if($productCustomOption['is_require'] == 1) { //if one is required, product should be set to required
+                        $items[$productId]['product']['required_options'] = 1;
+                    }
+
+                    foreach($productCustomOption['titles'] as $productCustomOptionTitle) {
+                        $items[$productId]['titles'][] = array(
+                            'option_id' => $nextOptionId,
+                            'store_id' => $productCustomOptionTitle['store_id'],
+                            'title' => $productCustomOptionTitle['title']
                         );
                     }
 
-                    foreach($value['prices'] as $valuePrice) {
-                        $items[$productId]['values_prices'][] = array(
-                            'option_type_id' => $nextValueId,
-                            'store_id' => $valuePrice['store_id'],
-                            'price' => $valuePrice['price'],
-                            'price_type' => $valuePrice['price_type'],
+                    foreach($productCustomOption['prices'] as $productCustomOptionPrice) {
+                        $items[$productId]['prices'][] = array(
+                            'option_id' => $nextOptionId,
+                            'store_id' => $productCustomOptionPrice['store_id'],
+                            'price' => $productCustomOptionPrice['price'],
+                            'price_type' => $productCustomOptionPrice['price_type']
                         );
                     }
+
+                    foreach($productCustomOption['values'] as $value) {
+
+                        if(isset($value['option_type_id'])){
+                            $nextValueId = $value['option_type_id'];
+                        } else {
+                            $nextValueId = $nextAutoValueId++;
+                        }
+
+                        $items[$productId]['values'][] = array(
+                            'option_type_id' => $nextValueId,
+                            'option_id' => $nextOptionId,
+                            'sku' => $value['sku'],
+                            'sort_order' => $value['sort_order']
+                        );
+
+                        foreach($value['titles'] as $valueTitle) {
+                            $items[$productId]['values_titles'][] = array(
+                                'option_type_id' => $nextValueId,
+                                'store_id' => $valueTitle['store_id'],
+                                'title' => $valueTitle['title'],
+                            );
+                        }
+
+                        foreach($value['prices'] as $valuePrice) {
+                            $items[$productId]['values_prices'][] = array(
+                                'option_type_id' => $nextValueId,
+                                'store_id' => $valuePrice['store_id'],
+                                'price' => $valuePrice['price'],
+                                'price_type' => $valuePrice['price_type'],
+                            );
+                        }
+                    }
                 }
+
+                $result[] = $productId;
             }
 
-            $result[] = $productId;
+            if( $productCustomOptions['remove'] ){
+                foreach ($productCustomOptions['remove'] as $optionId){
+                    $remove[] = $optionId;
+                }
+
+            }
         }
 
-        foreach($items as $productId => $item) {
-            $this->connection->delete($optionTable, $this->connection->quoteInto('product_id = ?', $productId));
-            $this->connection->insertOnDuplicate($productTable, $item['product'], array('has_options', 'required_options', 'updated_at'));
-            if($item['options'] && count($item['options']) > 0) {
-                $this->connection->insertOnDuplicate($optionTable, $item['options']);
-                $this->connection->insertOnDuplicate($titleTable, $item['titles'], array('title'));
-                if($item['prices'] && count($item['prices']) > 0) {
-                    $this->connection->insertOnDuplicate($priceTable, $item['prices'], array('price', 'price_type'));
-                }
-                if($item['values'] && count($item['values']) > 0) {
-                    $this->connection->insertOnDuplicate($typeValueTable, $item['values'], array('sku', 'sort_order'));
-                    $this->connection->insertOnDuplicate($typeTitleTable, $item['values_titles'], array('title'));
-                    $this->connection->insertOnDuplicate($typePriceTable, $item['values_prices'], array('price', 'price_type'));
+        if( isset( $items ) ){
+            foreach( $items as $productId => $item ) {
+                $this->connection->insertOnDuplicate($productTable, $item['product'], array('has_options', 'required_options', 'updated_at'));
+                if($item['options'] && count($item['options']) > 0) {
+                    $this->connection->insertOnDuplicate($optionTable, $item['options']);
+                    $this->connection->insertOnDuplicate($titleTable, $item['titles'], array('title'));
+                    if($item['prices'] && count($item['prices']) > 0) {
+                        $this->connection->insertOnDuplicate($priceTable, $item['prices'], array('price', 'price_type'));
+                    }
+                    if($item['values'] && count($item['values']) > 0) {
+                        $this->connection->insertOnDuplicate($typeValueTable, $item['values'], array('sku', 'sort_order'));
+                        $this->connection->insertOnDuplicate($typeTitleTable, $item['values_titles'], array('title'));
+                        $this->connection->insertOnDuplicate($typePriceTable, $item['values_prices'], array('price', 'price_type'));
+                    }
                 }
             }
+        }
+
+        if (isset ($remove)){
+            foreach ($remove as $optionId) {
+                $this->connection->delete($optionTable, array(
+                    $this->connection->quoteInto('product_id = ?', $productId),
+                    $this->connection->quoteInto('option_id = ?', $optionId)
+                    )
+                );
+            }
+
         }
 
         $this->touchProducts($changedProductIds);
